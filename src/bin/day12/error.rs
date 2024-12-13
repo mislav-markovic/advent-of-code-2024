b607ed0fcc4772f52322148baa4eb8142c89828e
Mislav Markovic <markovic.mislav@proton.me>
2024-12-13 17:25:27 +0100

    Day 12 Prepare

---
+++ src/bin/day12/error.rs
@@ -0,0 +1,7 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub(crate) enum Day12Error {
+    #[error("could not parse '{input}' into garden setup: {error_msg}")]
+    GardenParseError { input: String, error_msg: String },
+}
